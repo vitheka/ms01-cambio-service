b493af73a96bd8ff8bf832447735edd739e46253
vitheka <vitorcoelhoft@gmail.com>
2024-04-17 22:39:19 -0300

    Implements OpenAPI and config

--- src/main/java/br/com/vitor/ms01cambioservice/controller/CambioController.java
+++ src/main/java/br/com/vitor/ms01cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.vitor.ms01cambioservice.controller;
 
 import br.com.vitor.ms01cambioservice.repository.CambioRepository;
 import br.com.vitor.ms01cambioservice.domain.Cambio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.core.env.Environment;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Tag(name = "Cambio endpoint")
 @RestController
 @RequestMapping("/v1/cambio-service")
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ public class CambioController {
     private final Environment environment;
     private final CambioRepository repository;
 
+    @Operation(summary = "Find a specific book by you id")
     @GetMapping("/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
                             @PathVariable("from") String from,
